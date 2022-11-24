23ff01ffaf02e02519219c635b624a1eb4b474d8
Nisha G Kamath <117295761+gnishakamath-snowflake@users.noreply.github.com>
2022-11-24 19:37:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,72 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('Zena\'s Amazing Athleisure Catalog')
-# connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# run a snowflake query and put it all in a var called my_catalog
-my_cur.execute("select color_or_style from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website")
-my_catalog = my_cur.fetchall()
-# put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
-# temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
-# put the first column into a list
-color_list = df[0].values.tolist()
-# print(color_list)
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
-# We'll build the image caption now, since we can
-product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
-# use the option selected to go back and get all the info from the database
-my_cur.execute("select direct_url, price, size_list, upsell_product_desc from ZENAS_ATHLEISURE_DB.PRODUCTS.catalog_for_website where color_or_style = '" + option + "';")
-df2 = my_cur.fetchone()
-streamlit.image(
-df2[0],
-width=400,
-caption= product_caption
-)
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+def get_fv_data(fv_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fv_choice)
+  #streamlit.text(fruityvice_response.json()) #Just displays data in JSON 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Takes JSON output and normaizes the same 
+  return(fruityvice_normalized)
+
+def get_sf_FRUIT_LOAD_LIST():
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+  #my_data_row = my_cur.fetchone()
+  my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+  my_cnx.close()
+  return(my_cur.fetchall())  
+  
+def put_sf_FRUIT_LOAD_LIST(fv_val):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('"+fv_val+"')")
+  my_cnx.close()
+  return("Thanks for adding "+fv_val+" to the list!")  
+
+
+streamlit.title("My Mom's New Healthy Diner")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
+# Display the table on the page
+#streamlit.dataframe(my_fruit_list) 
+
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get the information")
+  else:
+    fv_res = get_fv_data(fruit_choice)
+    #streamlit.write('The user entered ', fruit_choice)    
+    streamlit.dataframe(fv_res) # displays the data in tabular format
+except URLError as e:
+  streamlit.error()
+  
+
+
+streamlit.header("View Our Fruit List - Add Your Favourites!")
+if streamlit.button('Get Fruit List'):
+  fv_list = get_sf_FRUIT_LOAD_LIST()
+  streamlit.dataframe(fv_list)
+
+#streamlit.stop()
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add fruit to the List'):
+  fv_add = put_sf_FRUIT_LOAD_LIST(add_my_fruit)
+  streamlit.text(fv_add)
