ca57f039dbb2c1dedc66722bb8f58425b67941b0
Nisha G Kamath <117295761+gnishakamath-snowflake@users.noreply.github.com>
2022-11-24 19:35:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
+"""
+Comment
+"""
+
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
